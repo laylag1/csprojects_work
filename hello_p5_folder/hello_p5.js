c28d0bffabceec8f4053f2cb6e25557b3b3cf4b5
laylag22 <laylagoodarzi22@marlborough.org>
2021-09-08 14:39:16 -0700

    Update hello_p5.js

--- hello_p5_folder/hello_p5.js
+++ hello_p5_folder/hello_p5.js
@@ -1,12 +1,7 @@
 function setup() {
   createCanvas(400, 400);
 
-  for(let i=0; i<10; i++){
-    fill(random(256),random(256),random(256));
-    let x1 = random(0,width);
-    let y1 = random(0,height);
-    rect(x1,y1,50,50);
-  }
+
 }
 
 let x = mouseX;
@@ -24,7 +19,12 @@ function draw() {
   }else if(w < 50){
     w+=wSize;
   }
-
+  for(let i=0; i<10; i++){
+    fill(random(256),random(256),random(256));
+    let x1 = random(0,width);
+    let y1 = random(0,height);
+    rect(x1,y1,50,50);
+  }
 }
 
 function mousePressed(){
